ca0f35d00b068eeeeeb2243912b5a3449d9f0ea3
Richard Paleczny <palecznyr@yahoo.com>
2016-05-03 19:08:32 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,7 +2,7 @@ source("reading_the_data.R")
 png(filename = "plot2.png", 
     width = 480, height = 480,
     units = "px")
-plot(DateTime, Global_active_power, 
+plot(Date_Time, Global_active_power, 
      type = "l",
      xlab = "",
      ylab = "Global Active Power (kilowatts)")
